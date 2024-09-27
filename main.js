ef676edf54013deb363d626bb65f7c066d007378
Gensy12 <morillogensy33@gmail.com>
2024-09-27 03:00:30 +0200

    v1
    v1

---
+++ main.js
@@ -0,0 +1,18 @@
+// Declarar la variable
+const nu1 = prompt("Ingresa el numero  1")
+const nu2 = prompt("Ingresa el numero  2")
+const nu3 = prompt("Ingresa el numero  3")
+
+// convertir a entero 
+const nu1Int = Number(nu1)
+const nu2Int = Number(nu2) 
+const nu3int = Number(nu3)
+
+// sumar los numero
+const resultado = nu1Int + nu2Int + nu3Int 
+
+// calcular promedio
+const promedio = resultado / 3
+
+//mostrar el resultado
+alert(" promedio de los tres numero es: " + promedio ) 
